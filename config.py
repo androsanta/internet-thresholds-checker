623629a2d386d2ff6d712db89775c99b8458d1e0
Andrea Santu <andreasantu@gmail.com>
2019-11-09 23:45:12 +0100

    Fixed configuration file

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 config = {
     'web_cube_username': os.environ['USERNAME'],
     'web_cube_password': os.environ['PASSWORD'],
-    'db_host': os.environ['DATABASE'] if os.environ['DATABASE'] else 'localhost'
+    'db_host': os.environ['DATABASE'] if os.environ.get('DATABASE') else 'localhost'
 }
